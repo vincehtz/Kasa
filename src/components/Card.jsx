7e3bf6c819f2ab7a83fb02fa1724ab8cd569759c
Vincent Hertz <vincent.hertz@gmail.com>
2024-02-17 00:46:30 +0100

    ajout du linear gradient sur les cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import "../styles/Card.scss";
 
 function Card({ name, cover }) {
   const cardStyle = {
-    backgroundImage: `url(${cover})`,
+    backgroundImage: `linear-gradient(rgba(255, 255, 255, 0), rgba(10, 10, 10, 0), rgba(4, 4, 4, 0.2), rgba(0, 0, 0, 0.5)), url(${cover})`,
   };
 
   return (
